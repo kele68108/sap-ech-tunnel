20e3fd6bb97f064ec82c2dd05eb9fb74be118594
kele68108 <kele68108@gmail.com>
2026-04-03 23:02:02 +0800

    Update ech-tunnel.sh

--- ech-tunnel.sh
+++ ech-tunnel.sh
@@ -6,7 +6,7 @@
 # =========================================================
 
 # --- 全局变量 ---
-GITHUB_BIN_URL="https://github.com/kele35818/nodejs/raw/refs/heads/main/ech-tunnel-linux-amd64"
+GITHUB_BIN_URL="https://github.com/kele68108/sap-ech-tunnel/raw/refs/heads/main/ech-tunnel-linux-amd64"
 BIN_PATH="/usr/local/bin/ech-tunnel"
 CONF_BASE_DIR="/etc/ech-tunnel"
 SHORTCUT_CMD="/usr/bin/ech"
